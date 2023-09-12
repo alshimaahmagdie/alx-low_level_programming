92b6a59a839f5e899b648bb24cc2b47f2ad31a73
Alshimaa Magdie Hassan <alshimaa.mhassan92@gmail.com>
2023-09-11 18:31:00 -0700

    task

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: prints all single digit numbers of base 10 starting from 0
+ * Description: prints all single digit numbers
  *
  * Return: 0 (success)
 */

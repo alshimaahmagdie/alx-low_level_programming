fcaff0f05767bfc5fbd1129bef9e355e7a3092a6
Alshimaa Magdie Hassan <alshimaa.mhassan92@gmail.com>
2023-09-11 18:25:52 -0700

    task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		printf("%i", digit);
+		digit++;
+	}
+	printf('\n');
+
+	return (0);
+}

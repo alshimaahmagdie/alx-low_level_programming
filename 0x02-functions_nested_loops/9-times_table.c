8a1c2ee85364cb4fb42294b8f4da0cae7956e6cf
Alshimaa Magdie Hassan <alshimaa.mhassan92@gmail.com>
2023-09-12 22:06:18 -0700

    task

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,16 +18,16 @@ void time_table(void)
 		_putchar(48);
 		for (mult = 1; mult <= 9 ; mult++)
 		{
-		_putchar(',');
-		_putchar(' ');
-
-		prod = num * mult;
-		if (prod <= 9)
+			_putchar(',');
 			_putchar(' ');
-		else
-			_putchar((prod / 10) + 48);
 
-		_putchar((prod % 10) + 48);
+			prod = num * mult;
+
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + 48);
+			_putchar((prod % 10) + 48);
 		}
 		_putchar('\n');
 	}
